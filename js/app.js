f3ff4505eb7a288667b5045140e96fc20b635bf8
keshibat <kensukeshibata@.com>
2018-08-08 12:54:07 +0800

    Add Map (Not working yet)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 function toggleSidebar() {
     document.getElementById("sidebar").classList.toggle('active');
 }
+
+var map;
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7413549, lng: -73.9980244},
+    zoom: 13,
+    mapTypeControl: false
+  });
+}
